a4ed50b074e2f372a87e1294faac5b89543c93bb
maxkulbako <mkulbako@gmail.com>
2025-01-16 12:47:48 +0100

    add loader and message components

--- client/src/pages/ProductPage.jsx
+++ client/src/pages/ProductPage.jsx
@@ -1,7 +1,8 @@
-import { useParams } from "react-router";
-import { Link } from "react-router";
+import { useParams, Link } from "react-router";
 import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap";
 import Rating from "../components/Rating";
+import Loader from "../components/Loader";
+import Message from "../components/Message";
 import { useGetProductDetailsQuery } from "../slices/productsApiSlice";
 
 const ProductPage = () => {
@@ -19,9 +20,11 @@ const ProductPage = () => {
         Go Back
       </Link>
       {isLoading ? (
-        <h2>Loading...</h2>
+        <Loader />
       ) : error ? (
-        <div>{error?.data?.message || error.error}</div>
+        <Message variant="danger">
+          {error?.data?.message || error.error}
+        </Message>
       ) : (
         <>
           {" "}
