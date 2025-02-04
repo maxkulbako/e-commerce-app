86f4f03afc8a854b5a0b56ebe13ae47f97f005d5
maxkulbako <mkulbako@gmail.com>
2025-02-04 20:02:23 +0100

    fix: delete double loader on homepage

--- client/src/components/ProductCarousel.jsx
+++ client/src/components/ProductCarousel.jsx
@@ -1,29 +1,27 @@
 import { Link } from "react-router";
 import { Carousel, Image } from "react-bootstrap";
-import Loader from "./Loader";
-import Message from "./Message";
 import { useGetTopProductsQuery } from "../slices/productsApiSlice";
 
 const ProductCarousel = () => {
-  const { data: products, isLoading, error } = useGetTopProductsQuery();
+  const { data: products } = useGetTopProductsQuery();
 
-  return isLoading ? (
-    <Loader />
-  ) : error ? (
-    <Message variant="danger">{error.data.message || error.error}</Message>
-  ) : (
-    <Carousel pause="hover" className="bg-dark mt-3">
-      {products.map((product) => (
-        <Carousel.Item key={product._id}>
-          <Link to={`/product/${product._id}`}>
-            <Image src={product.image} alt={product.name} fluid />
-            <Carousel.Caption className="carousel-caption">
-              <h2>{product.name}</h2>
-            </Carousel.Caption>
-          </Link>
-        </Carousel.Item>
-      ))}
-    </Carousel>
+  return (
+    <>
+      {products && (
+        <Carousel pause="hover" className="bg-dark mt-3">
+          {products.map((product) => (
+            <Carousel.Item key={product._id}>
+              <Link to={`/product/${product._id}`}>
+                <Image src={product.image} alt={product.name} fluid />
+                <Carousel.Caption className="carousel-caption">
+                  <h2>{product.name}</h2>
+                </Carousel.Caption>
+              </Link>
+            </Carousel.Item>
+          ))}
+        </Carousel>
+      )}
+    </>
   );
 };
 
